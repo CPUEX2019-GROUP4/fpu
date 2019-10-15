a670c3d501a8aab2d510735cca2322abb75879a1
ikeda-r <ryo40160@gmail.com>
2019-10-15 14:00:43 +0900

    fix ftoi

--- ftoi.sv
+++ ftoi.sv
@@ -6,12 +6,12 @@ module ftoi (
     wire [7:0] e = x[30:23];
     wire [22:0] m = x[22:0];
 
-    wire e_unbised_plus_1 [4:0] = (e - 8'd126)[4:0];
+    wire [4:0] e_unbised_plus_1 = (e - 8'd126)[4:0];
 
-    wire ext_abs_ans [54:0] = ({32'b1, m} << e_unbised_plus_1);
+    wire [54:0] ext_abs_ans = ({32'b1, m} << e_unbised_plus_1);
 
-    wire abs_ans [30:0] = ext_abs_ans[54:24] + {30'b0, ext_abs_ans[23]};
-    wire ans [31:0] = s ? (~abs_ans + 31'b0) : abs_ans;
+    wire [30:0] abs_ans = ext_abs_ans[54:24] + {30'b0, ext_abs_ans[23]};
+    wire [31:0] ans = s ? (~abs_ans + 31'b0) : abs_ans;
 
     assign y = // e >= 158 (|x| >= 2^31) のときは int_max or int_min を返す
         e <= 8'd125
