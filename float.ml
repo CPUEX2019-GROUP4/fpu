ffa0df34a113d6f1eeb6d76394b6e3ead2c098aa
ikeda-r <ryo40160@gmail.com>
2019-10-31 20:06:58 +0900

    inspected precision

--- float.ml
+++ float.ml
@@ -1,17 +1,15 @@
 (** float ライブラリ
- *  精度未保証
  *)
 
 (** 円周率
  *)
 let pi = 3.14159265
 
-(* ////////// 適当に決めただけ ここから ////////// *)
-let finv_loop_count = 7
-let sqrt_loop_count = 7
-let sin_loop_count = 10
-let atan_loop_count = 10
-(* ////////// 適当に決めただけ ここまで ////////// *)
+let finv_loop_count = 4
+let sqrt_loop_count = 4
+let sin_loop_count = 5
+let cos_loop_count = 5 (* pi / 2 付近の精度に問題 *)
+let atan_loop_count = 7 (* sqrt 2 - 1 付近の精度に問題 *)
 
 (* ////////// bit 演算を使うかハードウェアで実装 ここから ////////// *)
 (* e = (127 - e_of_x) + 127 *)
@@ -23,12 +21,20 @@ external sqrt_init : float -> float
 let rec sin_taylor_term_0 x = x
 
 (* i >= 1 *)
-let rec sin_taylor_term x prev i = -.prev *. x *. x /. (2. *. (float_of_int i) *. (2. *. (float_of_int i) +. 1.))
+let rec sin_taylor_term x prev i =
+    -.prev *. x *. x /. (2. *. (float_of_int i) *. (2. *. (float_of_int i) +. 1.))
+
+let rec cos_taylor_term_0 x = 1.
+
+(* i >= 1 *)
+let rec cos_taylor_term x prev i =
+    -.prev *. x *. x /. (2. *. (float_of_int i) *. (2. *. (float_of_int i) -. 1.))
 
 let rec atan_taylor_term_0 x = x
 
 (* i >= 1 *)
-let rec atan_taylor_term x prev i = -.prev *. x *. x *. (2. *. (float_of_int i) -. 1.) /. (2. *. (float_of_int i) +. 1.)
+let rec atan_taylor_term x prev i =
+    -.prev *. x *. x *. (2. *. (float_of_int i) -. 1.) /. (2. *. (float_of_int i) +. 1.)
 
 let rec taylor x n init term_iter =
     let rec inner i prev acc =
@@ -48,7 +54,11 @@ let rec finv x =
             t
         else
             inner (t *. (2. -. x *. t)) (i - 1)
-    in inner (finv_init x) finv_loop_count
+    in
+        if fiszero x then
+            0.
+        else
+            inner (finv_init x) finv_loop_count
 
 (** x を y で除算する
  *  y の逆数を x に乗算する
@@ -64,7 +74,11 @@ let rec sqrt x =
             t
         else
             inner ((t *. t +. x) /. (2. *. t)) (i - 1)
-    in inner (sqrt_init x) sqrt_loop_count
+    in
+        if fiszero x then
+            0.
+        else
+            inner (sqrt_init x) sqrt_loop_count
 
 (** x の sin を求める
  *  テーラー展開による
@@ -86,20 +100,73 @@ let rec sin x =
             pi -. lim_pi_x
         else
             lim_pi_x
-    in let lim_hpi_sin =
-        if lim_hpi_x <= 0. then
-            0.
-        else if lim_hpi_x >= pi /. 2. then
-            1.
+    in let (lim_hhpi_x, usecos) =
+        if lim_hpi_x > pi /. 4. then
+            (pi /. 2. -. lim_hpi_x, true)
         else
-            taylor lim_hpi_x sin_loop_count
-                (sin_taylor_term_0 lim_hpi_x) sin_taylor_term
+            (lim_hpi_x, false)
+    in let lim_hpi_sin =
+        if usecos then
+            if lim_hhpi_x <= 0. then
+                1.
+            else if lim_hhpi_x >= pi /. 4. then
+                sqrt 2. /. 2.
+            else
+                taylor lim_hhpi_x cos_loop_count
+                    (cos_taylor_term_0 lim_hhpi_x) cos_taylor_term
+        else
+            if lim_hhpi_x <= 0. then
+                0.
+            else if lim_hhpi_x >= pi /. 4. then
+                sqrt 2. /. 2.
+            else
+                taylor lim_hhpi_x sin_loop_count
+                    (sin_taylor_term_0 lim_hhpi_x) sin_taylor_term
     in if neg1 = neg2 then lim_hpi_sin else -.lim_hpi_sin
 
 (** x の cos を求める
- *  sin の位相をずらすことによる
+ *  テーラー展開による
  *)
-let rec cos x = sin (x +. pi /. 2.)
+let rec cos x =
+    let abs_x =
+        if x < 0. then
+            -.x
+        else
+            x
+    in let lim_2pi_x = abs_x -. (2. *. pi) *. floor (abs_x /. (2. *. pi))
+    in let lim_pi_x =
+        if lim_2pi_x > pi then
+            2. *. pi -. lim_2pi_x
+        else
+            lim_2pi_x
+    in let (lim_hpi_x, neg) =
+        if lim_pi_x > pi /. 2. then
+            (pi -. lim_pi_x, true)
+        else
+            (lim_pi_x, false)
+    in let (lim_hhpi_x, usesin) =
+        if lim_hpi_x > pi /. 4. then
+            (pi /. 2. -. lim_hpi_x, true)
+        else
+            (lim_hpi_x, false)
+    in let lim_hpi_cos =
+        if usesin then
+            if lim_hhpi_x <= 0. then
+                0.
+            else if lim_hhpi_x >= pi /. 4. then
+                sqrt 2. /. 2.
+            else
+                taylor lim_hhpi_x sin_loop_count
+                    (sin_taylor_term_0 lim_hhpi_x) sin_taylor_term
+        else
+            if lim_hhpi_x <= 0. then
+                1.
+            else if lim_hhpi_x >= pi /. 4. then
+                sqrt 2. /. 2.
+            else
+                taylor lim_hhpi_x cos_loop_count
+                    (cos_taylor_term_0 lim_hhpi_x) cos_taylor_term
+    in if neg then -.lim_hpi_cos else lim_hpi_cos
 
 (** x の atan を求める
  *  テーラー展開による
@@ -110,24 +177,21 @@ let rec atan x =
             (-.x, true)
         else
             (x, false)
-    in let (lim_r1_x, trans) =
-        if abs_x > 1. then
-            (finv abs_x, true)
+    in let (lim_r2m1_x, trans) =
+        if abs_x > sqrt 2. +. 1. then
+            (finv abs_x, 2)
+        else if abs_x > sqrt 2. -. 1. then
+            ((1. -. abs_x) /. (1. +. abs_x), 1)
         else
-            (abs_x, false)
-    in let (lim_r21_x, trans2) =
-        if lim_r1_x > sqrt 2. -. 1. then
-            ((1. -. lim_r1_x) /. (1. +. lim_r1_x), true)
+            (abs_x, 0)
+    in let lim_r2m1_atan =
+        taylor lim_r2m1_x atan_loop_count
+                (atan_taylor_term_0 lim_r2m1_x) atan_taylor_term
+    in let lim_pos_atan =
+        if trans = 2 then
+            pi /. 2. -. lim_r2m1_atan
+        else if trans = 1 then
+            pi /. 4. -. lim_r2m1_atan
         else
-            (lim_r1_x, false)
-    in let lim_r21_atan =
-        if lim_r21_x <= 0. then
-            0.
-        else if lim_r21_x >= 1. then
-            1.
-        else
-            taylor lim_r21_x atan_loop_count
-                (atan_taylor_term_0 lim_r21_x) atan_taylor_term
-    in let lim_r1_atan = if trans2 then pi /. 4. -. lim_r21_atan else lim_r21_atan
-    in let lim_pos_atan = if trans then pi /. 2. -. lim_r1_atan else lim_r1_atan
+            lim_r2m1_atan
     in if neg then -.lim_pos_atan else lim_pos_atan
