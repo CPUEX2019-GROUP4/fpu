452344226a8bd5f892d4dbc0b702ffc7697c2235
ikeda-r <ryo40160@gmail.com>
2019-11-08 01:51:28 +0900

    changed spec of ftoi

--- fpu/ftoi.sv
+++ fpu/ftoi.sv
@@ -12,17 +12,17 @@ module ftoi (
     wire [7:0] e = x[30:23];
     wire [22:0] m = x[22:0];
 
-    wire [7:0] e_unbised_plus_1_8 = e - 8'd126;
+    wire [7:0] e_unbised_8 = e - 8'd127;
 
-    wire [4:0] e_unbised_plus_1 = e_unbised_plus_1_8[4:0];
+    wire [4:0] e_unbised = e_unbised_8[4:0];
 
-    wire [54:0] ext_abs_ans = ({32'b1, m} << e_unbised_plus_1);
+    wire [53:0] ext_abs_ans = ({31'b1, m} << e_unbised);
 
-    wire [30:0] abs_ans = ext_abs_ans[54:24] + {30'b0, ext_abs_ans[23]};
+    wire [30:0] abs_ans = ext_abs_ans[53:23];
     wire [31:0] ans = s ? (~{1'b0, abs_ans} + 32'b1) : {1'b0, abs_ans};
 
     assign y = // e >= 158 (|x| >= 2^31) のときは int_max or int_min を返す
-        e <= 8'd125
+        e <= 8'd126
             ? 32'b0
             : (e >= 8'd158
                 ? (s
