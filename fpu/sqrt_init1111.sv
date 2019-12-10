2c0a430ac7a7c07dec6284ad0ac287656aa0c69d
ikeda-r <ryo40160@gmail.com>
2019-12-10 14:42:07 +0900

    share

--- fpu/sqrt_init1111.sv
+++ fpu/sqrt_init1111.sv
@@ -8,12 +8,12 @@ module sqrt_init (
     wire [22:0] m = x[22:0];
 
     wire ys = s;
-    wire [7:0] ye = 8'd189 - ((e - 8'd1) >> 1) + {7'b0, m[22:17] == '0 && e[0]};
+    wire [7:0] ye = 8'd189 - ((e - 8'd1) >> 1) + {7'b0, m[22:12] == '0 && e[0]};
     wire [22:0] ym;
 
     sqrt_init_m sqrt_init_m0({e[0], m[22:12]}, ym[22:12]);
 
-    assign ym[16:0] = '0;
+    assign ym[11:0] = '0;
 
     assign y = e == '0 ? 32'b0 : {ys, ye, ym};
 
